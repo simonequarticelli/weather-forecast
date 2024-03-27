8ca6d436c6b58f1cfd23242b87e241939b964c33
Quarticelli <>
2024-03-28 00:09:44 +0100

    Add Vuexy template and set dashborad page with chartjs lib

---
+++ src/navigation/vertical/index.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: 'Dashboard',
+    to: { name: 'index' },
+    icon: { icon: 'tabler-smart-home' },
+  },
+  {
+    title: 'Documentation',
+    to: { name: 'documentation' },
+    icon: { icon: 'tabler-file' },
+  },
+]
