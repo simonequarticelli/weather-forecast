68f933bad30c262c4673a54b2f11a608d865c113
Quarticelli <>
2024-03-28 20:47:18 +0100

    Refactored index.vue, enhancing functionalities and adding weather filters

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,10 +4,11 @@ import ChartJsBubbleChart from '@/views/chartjs/ChartJsBubbleChart.vue'
 import ChartJsHorizontalBarChart from '@/views/chartjs/ChartJsHorizontalBarChart.vue'
 import ChartJsLineAreaChart from '@/views/chartjs/ChartJsLineAreaChart.vue'
 import ChartJsLineChart from '@/views/chartjs/ChartJsLineChart.vue'
-import ChartJsPolarAreaChart from '@/views/chartjs/ChartJsPolarAreaChart.vue'
 import ChartJsRadarChart from '@/views/chartjs/ChartJsRadarChart.vue'
 import ChartJsScatterChart from '@/views/chartjs/ChartJsScatterChart.vue'
+import ChartJsPolarAreaChart from '@/views/chartjs/ChartJsPolarAreaChart.vue'
 import { openWeatherRepository } from "@/repositories/openWeatherRepository"
+import { integerValidator, lengthValidator, requiredValidator } from "@validators"
 
 const chartJsCustomColors = {
   white: '#fff',
@@ -30,214 +31,370 @@ const chartJsCustomColors = {
   scatterChartWarning: '#ff9f43',
 }
 
-const city = ref('torino')
-const openWeatherResponse = ref([])
+const refVForm = ref()
+const processing = ref(false)
+const city = ref('')
+const formError = ref(null)
+const isFlatSnackbarVisible = ref(false)
 
-onMounted(() => {
-  openWeatherRepository().index({
-    q: city.value,
-  }).then(response => {
-    console.log(response.data)
-    openWeatherResponse.value = response.data
-  })
+const openWeatherResponse = reactive({
+  snow: '',
+  rain: '',
+  clouds: '',
+  humidity: '',
 })
+
+const languages = [
+  {
+    language: 'English',
+    abbr: 'EN',
+  },
+  {
+    language: 'Italiano',
+    abbr: 'IT',
+  },
+]
+
+const selectedLanguage = ref({
+  language: 'English',
+  abbr: 'EN',
+})
+
+const filters = reactive({
+  weathering: true,
+  sunriseSunset: false,
+  temperature: false,
+})
+
+const request = reactive({
+  q: 'Torino',
+  zip: '',
+  lang: computed(() => selectedLanguage.value.abbr.toLowerCase()),
+})
+
+const onReset = () => {
+  formError.value = null
+  refVForm.value?.reset()
+}
+
+onMounted(() => onSubmit())
+
+const onSubmit = () => {
+  formError.value = null
+  refVForm.value?.validate().then(({ valid: isValid }) => {
+    if (isValid) {
+      processing.value = true
+      setTimeout(() => {
+        openWeatherRepository().index(request).then(response => {
+          console.log(response.data)
+          openWeatherResponse.clouds = response.data.clouds.all
+          openWeatherResponse.humidity = response.data.main.humidity
+          openWeatherResponse.rain = response.data?.rain
+          openWeatherResponse.snow = response.data?.snow
+        }).catch(error => {
+          formError.value = error.response.data
+          isFlatSnackbarVisible.value = true
+        }).finally(() => {
+          processing.value = false
+          city.value = null
+        })
+      }, 1000)
+    }
+  })
+}
 </script>
 
 <template>
   <VRow id="chartjs-wrapper">
     <VCol cols="12">
-      <pre>
-        {{ openWeatherResponse }}
-      </pre>
-      <VForm @submit.prevent="() => {}">
+      <VForm
+        ref="refVForm"
+        @submit.prevent="onSubmit"
+      >
         <VRow>
-          <VCol
-            cols="12"
-            md="6"
-          >
-            <VCol cols="12">
-              <AppTextField
-                v-model="city"
-                label="City"
-                placeholder="City"
-              />
-            </VCol>
-            <VCol
-              cols="12"
-              class="d-flex gap-4"
+          <VCol cols="12">
+            <!-- 👉 Flat snackbar -->
+            <VSnackbar
+              v-model="isFlatSnackbarVisible"
+              location="top end"
+              :timeout="2000"
+              variant="flat"
+              color="error"
             >
-              <VBtn type="submit">
-                Search
-              </VBtn>
+              {{ formError?.message }}
+            </VSnackbar>
 
-              <VBtn
-                type="reset"
-                color="secondary"
-                variant="tonal"
+            <VRow>
+              <!-- 👉 Search bar -->
+              <VCol cols="12">
+                {{ openWeatherResponse }}
+                <AppTextField
+                  v-model="request.q"
+                  autofocus
+                  label="City"
+                  placeholder="City"
+                />
+              </VCol>
+              <!-- 👉 Postal code -->
+              <VCol cols="12">
+                <AppTextField
+                  v-model="request.zip"
+                  :rules="[integerValidator, lengthValidator(request.postalCode, 5)]"
+                  label="Postal code (works only USA locations)"
+                  placeholder="Posta code"
+                />
+              </VCol>
+              <!-- 👉 Languages -->
+              <VCol cols="12">
+                <AppSelect
+                  v-model="selectedLanguage"
+                  :hint="`${selectedLanguage.language}, ${selectedLanguage.abbr}`"
+                  :items="languages"
+                  item-title="language"
+                  item-value="abbr"
+                  label="Language"
+                  persistent-hint
+                  return-object
+                  single-line
+                />
+              </VCol>
+            </VRow>
+
+            <!-- 👉 Actions -->
+            <VRow>
+              <VCol
+                cols="12"
+                class="d-flex gap-4"
               >
-                Reset
-              </VBtn>
-            </VCol>
-          </VCol>
+                <VBtn
+                  type="submit"
+                  :loading="processing"
+                  :disabled="processing"
+                >
+                  Search
+                </VBtn>
 
+                <VBtn
+                  color="secondary"
+                  variant="tonal"
+                  @click="onReset"
+                >
+                  Reset
+                </VBtn>
+              </VCol>
+            </VRow>
+
+            <!-- 👉 Filters -->
+            <VRow>
+              <VCol cols="12">
+                <VCheckbox
+                  v-model="filters.weathering"
+                  label="Humidity/Clouds"
+                />
+                <VCheckbox
+                  v-model="filters.sunriseSunset"
+                  label="Sunrise/Sunset"
+                />
+                <VCheckbox
+                  v-model="filters.temperature"
+                  label="Temperature"
+                />
+              </VCol>
+            </VRow>
+          </VCol>
         </VRow>
       </VForm>
     </VCol>
 
-    <!-- 👉 Statistics Line Chart  -->
-    <VCol cols="12">
-      <VCard
-        title="Statistics"
-        subtitle="Commercial networks and enterprises"
+    <template v-if="! processing">
+      <!-- 👉 Humidity and Clouds Polar Area Chart -->
+      <VCol
+        v-if="filters.weathering"
+        cols="12"
+        md="4"
       >
-        <VCardText>
-          <ChartJsLineChart :colors="chartJsCustomColors" />
-        </VCardText>
-      </VCard>
-    </VCol>
+        <VCard title="Humidity and Clouds">
+          <VCardText>
+            <ChartJsPolarAreaChart
+              :colors="chartJsCustomColors"
+              :labels="['Humidity', 'Clouds']"
+              label-message="Percentage"
+              :data="[openWeatherResponse.humidity, openWeatherResponse.clouds]"
+              :background-color="[
+                chartJsCustomColors.primary,
+                chartJsCustomColors.lineChartWarning,
+              ]"
+            />
+          </VCardText>
+        </VCard>
+      </VCol>
+      <!-- 👉 Rain and Snow Polar Area Chart -->
+      <VCol
+        v-if="filters.weathering"
+        cols="12"
+        md="4"
+      >
+        <VCard title="Rain and Snow">
+          <VCardText>
+            <ChartJsPolarAreaChart
+              v-if="openWeatherResponse.rain || openWeatherResponse.snow"
+              :colors="chartJsCustomColors"
+              :labels="['Rain', 'Show']"
+              label-message="Volume for the last 1 hour, mm."
+              :data="[openWeatherResponse.rain, openWeatherResponse.snow]"
+              :background-color="[
+                chartJsCustomColors.areaChartBlue,
+                chartJsCustomColors.polarChartGrey,
+              ]"
+            />
+            <p v-else>Nothing to show</p>
+          </VCardText>
+        </VCard>
+      </VCol>
+    </template>
 
-    <!-- 👉 Radar Chart -->
-    <VCol
-      cols="12"
-      md="6"
-    >
-      <VCard title="Radar Chart">
-        <VCardText>
-          <ChartJsRadarChart />
-        </VCardText>
-      </VCard>
-    </VCol>
+<!--    &lt;!&ndash; 👉 Statistics Line Chart  &ndash;&gt;-->
+<!--    <VCol cols="12">-->
+<!--      <VCard-->
+<!--        title="Statistics"-->
+<!--        subtitle="Commercial networks and enterprises"-->
+<!--      >-->
+<!--        <VCardText>-->
+<!--          <ChartJsLineChart :colors="chartJsCustomColors" />-->
+<!--        </VCardText>-->
+<!--      </VCard>-->
+<!--    </VCol>-->
 
-    <!-- 👉 Average Skills Polar Area Chart -->
-    <VCol
-      cols="12"
-      md="6"
-    >
-      <VCard title="Average Skills">
-        <VCardText>
-          <ChartJsPolarAreaChart :colors="chartJsCustomColors" />
-        </VCardText>
-      </VCard>
-    </VCol>
+<!--    &lt;!&ndash; 👉 Radar Chart &ndash;&gt;-->
+<!--    <VCol-->
+<!--      cols="12"-->
+<!--      md="6"-->
+<!--    >-->
+<!--      <VCard title="Radar Chart">-->
+<!--        <VCardText>-->
+<!--          <ChartJsRadarChart />-->
+<!--        </VCardText>-->
+<!--      </VCard>-->
+<!--    </VCol>-->
 
-    <!-- 👉  Bubble Chart -->
-    <VCol cols="12">
-      <VCard title="Bubble Chart">
-        <template #append>
-          <span class="text-subtitle-2">$ 100,000</span>
-        </template>
+<!--    &lt;!&ndash; 👉  Bubble Chart &ndash;&gt;-->
+<!--    <VCol cols="12">-->
+<!--      <VCard title="Bubble Chart">-->
+<!--        <template #append>-->
+<!--          <span class="text-subtitle-2">$ 100,000</span>-->
+<!--        </template>-->
 
-        <VCardText>
-          <ChartJsBubbleChart :colors="chartJsCustomColors" />
-        </VCardText>
-      </VCard>
-    </VCol>
+<!--        <VCardText>-->
+<!--          <ChartJsBubbleChart :colors="chartJsCustomColors" />-->
+<!--        </VCardText>-->
+<!--      </VCard>-->
+<!--    </VCol>-->
 
-    <!-- 👉 New Product Data Scatter Chart -->
-    <VCol cols="12">
-      <VCard>
-        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-          <VCardTitle>New Product Data</VCardTitle>
+<!--    &lt;!&ndash; 👉 New Product Data Scatter Chart &ndash;&gt;-->
+<!--    <VCol cols="12">-->
+<!--      <VCard>-->
+<!--        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">-->
+<!--          <VCardTitle>New Product Data</VCardTitle>-->
 
-          <template #append>
-            <VBtnToggle
-              color="primary"
-              variant="outlined"
-              density="compact"
-            >
-              <VBtn>Daily</VBtn>
-              <VBtn>Monthly</VBtn>
-              <VBtn>Yearly</VBtn>
-            </VBtnToggle>
-          </template>
-        </VCardItem>
+<!--          <template #append>-->
+<!--            <VBtnToggle-->
+<!--              color="primary"-->
+<!--              variant="outlined"-->
+<!--              density="compact"-->
+<!--            >-->
+<!--              <VBtn>Daily</VBtn>-->
+<!--              <VBtn>Monthly</VBtn>-->
+<!--              <VBtn>Yearly</VBtn>-->
+<!--            </VBtnToggle>-->
+<!--          </template>-->
+<!--        </VCardItem>-->
 
-        <VCardText>
-          <ChartJsScatterChart :colors="chartJsCustomColors" />
-        </VCardText>
-      </VCard>
-    </VCol>
+<!--        <VCardText>-->
+<!--          <ChartJsScatterChart :colors="chartJsCustomColors" />-->
+<!--        </VCardText>-->
+<!--      </VCard>-->
+<!--    </VCol>-->
 
-    <!-- 👉 Data Science Area Line Chart -->
-    <VCol cols="12">
-      <VCard>
-        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-          <VCardTitle>
-            Data Science
-          </VCardTitle>
+<!--    &lt;!&ndash; 👉 Data Science Area Line Chart &ndash;&gt;-->
+<!--    <VCol cols="12">-->
+<!--      <VCard>-->
+<!--        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">-->
+<!--          <VCardTitle>-->
+<!--            Data Science-->
+<!--          </VCardTitle>-->
 
-          <template #append>
-            <div class="date-picker-wrapper">
-              <AppDateTimePicker
-                model-value="2022-06-09"
-                prepend-inner-icon="tabler-calendar"
-                density="compact"
-                :config="{ position: 'auto right' }"
-              />
-            </div>
-          </template>
-        </VCardItem>
+<!--          <template #append>-->
+<!--            <div class="date-picker-wrapper">-->
+<!--              <AppDateTimePicker-->
+<!--                model-value="2022-06-09"-->
+<!--                prepend-inner-icon="tabler-calendar"-->
+<!--                density="compact"-->
+<!--                :config="{ position: 'auto right' }"-->
+<!--              />-->
+<!--            </div>-->
+<!--          </template>-->
+<!--        </VCardItem>-->
 
-        <VCardText>
-          <ChartJsLineAreaChart :colors="chartJsCustomColors" />
-        </VCardText>
-      </VCard>
-    </VCol>
+<!--        <VCardText>-->
+<!--          <ChartJsLineAreaChart :colors="chartJsCustomColors" />-->
+<!--        </VCardText>-->
+<!--      </VCard>-->
+<!--    </VCol>-->
 
-    <!-- 👉 Latest Statistics -->
-    <VCol
-      cols="12"
-      md="6"
-    >
-      <VCard>
-        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-          <VCardTitle>Latest Statistics</VCardTitle>
+<!--    &lt;!&ndash; 👉 Latest Statistics &ndash;&gt;-->
+<!--    <VCol-->
+<!--      cols="12"-->
+<!--      md="6"-->
+<!--    >-->
+<!--      <VCard>-->
+<!--        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">-->
+<!--          <VCardTitle>Latest Statistics</VCardTitle>-->
 
-          <template #append>
-            <div class="date-picker-wrapper">
-              <AppDateTimePicker
-                model-value="2022-06-09"
-                prepend-inner-icon="tabler-calendar"
-                density="compact"
-                :config="{ position: 'auto right' }"
-              />
-            </div>
-          </template>
-        </VCardItem>
+<!--          <template #append>-->
+<!--            <div class="date-picker-wrapper">-->
+<!--              <AppDateTimePicker-->
+<!--                model-value="2022-06-09"-->
+<!--                prepend-inner-icon="tabler-calendar"-->
+<!--                density="compact"-->
+<!--                :config="{ position: 'auto right' }"-->
+<!--              />-->
+<!--            </div>-->
+<!--          </template>-->
+<!--        </VCardItem>-->
 
-        <VCardText>
-          <ChartJsBarChart :colors="chartJsCustomColors" />
-        </VCardText>
-      </VCard>
-    </VCol>
+<!--        <VCardText>-->
+<!--          <ChartJsBarChart :colors="chartJsCustomColors" />-->
+<!--        </VCardText>-->
+<!--      </VCard>-->
+<!--    </VCol>-->
 
-    <!-- 👉 Balance Horizontal Bar Chart -->
-    <VCol
-      cols="12"
-      md="6"
-    >
-      <VCard>
-        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">
-          <VCardTitle>Balance</VCardTitle>
-          <VCardSubtitle>$74,123</VCardSubtitle>
+<!--    &lt;!&ndash; 👉 Balance Horizontal Bar Chart &ndash;&gt;-->
+<!--    <VCol-->
+<!--      cols="12"-->
+<!--      md="6"-->
+<!--    >-->
+<!--      <VCard>-->
+<!--        <VCardItem class="d-flex flex-wrap justify-space-between gap-4">-->
+<!--          <VCardTitle>Balance</VCardTitle>-->
+<!--          <VCardSubtitle>$74,123</VCardSubtitle>-->
 
-          <template #append>
-            <div class="date-picker-wrapper">
-              <AppDateTimePicker
-                model-value="2022-06-09"
-                prepend-inner-icon="tabler-calendar"
-                density="compact"
-                :config="{ position: 'auto right' }"
-              />
-            </div>
-          </template>
-        </VCardItem>
+<!--          <template #append>-->
+<!--            <div class="date-picker-wrapper">-->
+<!--              <AppDateTimePicker-->
+<!--                model-value="2022-06-09"-->
+<!--                prepend-inner-icon="tabler-calendar"-->
+<!--                density="compact"-->
+<!--                :config="{ position: 'auto right' }"-->
+<!--              />-->
+<!--            </div>-->
+<!--          </template>-->
+<!--        </VCardItem>-->
 
-        <VCardText>
-          <ChartJsHorizontalBarChart :colors="chartJsCustomColors" />
-        </VCardText>
-      </VCard>
-    </VCol>
+<!--        <VCardText>-->
+<!--          <ChartJsHorizontalBarChart :colors="chartJsCustomColors" />-->
+<!--        </VCardText>-->
+<!--      </VCard>-->
+<!--    </VCol>-->
   </VRow>
 </template>
 
