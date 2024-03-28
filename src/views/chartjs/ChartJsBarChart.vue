9cb0d0e91f3eac6387fafad7a420ed521b058f03
Quarticelli <>
2024-03-28 23:38:18 +0100

    Refactor ChartJsBarChart.vue to use props for data and labels

--- src/views/chartjs/ChartJsBarChart.vue
+++ src/views/chartjs/ChartJsBarChart.vue
@@ -4,6 +4,14 @@ import { getLatestBarChartConfig } from '@core/libs/chartjs/chartjsConfig'
 import BarChart from '@core/libs/chartjs/components/BarChart'
 
 const props = defineProps({
+  labels: {
+    type: Array,
+    required: true,
+  },
+  data: {
+    type: Array,
+    required: true,
+  },
   colors: {
     type: null,
     required: true,
@@ -14,21 +22,7 @@ const vuetifyTheme = useTheme()
 const chartOptions = computed(() => getLatestBarChartConfig(vuetifyTheme.current.value))
 
 const data = {
-  labels: [
-    '7/12',
-    '8/12',
-    '9/12',
-    '10/12',
-    '11/12',
-    '12/12',
-    '13/12',
-    '14/12',
-    '15/12',
-    '16/12',
-    '17/12',
-    '18/12',
-    '19/12',
-  ],
+  labels: props.labels,
   datasets: [{
     maxBarThickness: 15,
     backgroundColor: props.colors.barChartYellow,
@@ -37,21 +31,7 @@ const data = {
       topRight: 15,
       topLeft: 15,
     },
-    data: [
-      275,
-      90,
-      190,
-      205,
-      125,
-      85,
-      55,
-      87,
-      127,
-      150,
-      230,
-      280,
-      190,
-    ],
+    data: props.data,
   }],
 }
 </script>
