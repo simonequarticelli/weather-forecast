57b8ba105cddc63a848034488085a9c2f53d43db
Quarticelli <>
2024-03-29 18:32:42 +0100

    Update codebase by deleting ChartJs component files

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,37 +1,25 @@
 <script setup>
-import ChartJsBarChart from '@/views/chartjs/ChartJsBarChart.vue'
-import ChartJsPolarAreaChart from '@/views/chartjs/ChartJsPolarAreaChart.vue'
+import ChartJsBarChart from '@/components/chartjs/ChartJsBarChart.vue'
+import ChartJsPolarAreaChart from '@/components/chartjs/ChartJsPolarAreaChart.vue'
 import { openWeatherRepository } from "@/repositories/openWeatherRepository"
-import { integerValidator, lengthValidator, requiredValidator } from "@validators"
-
-const chartJsCustomColors = {
-  white: '#fff',
-  yellow: '#ffe802',
-  primary: '#836af9',
-  areaChartBlue: '#2c9aff',
-  barChartYellow: '#ffcf5c',
-  polarChartGrey: '#4f5d70',
-  polarChartInfo: '#299aff',
-  lineChartYellow: '#d4e157',
-  polarChartGreen: '#28dac6',
-  lineChartPrimary: '#9e69fd',
-  lineChartWarning: '#ff9800',
-  horizontalBarInfo: '#26c6da',
-  polarChartWarning: '#ff8131',
-  scatterChartGreen: '#28c76f',
-  warningShade: '#ffbd1f',
-  areaChartBlueLight: '#84d0ff',
-  areaChartGreyLight: '#edf1f4',
-  scatterChartWarning: '#ff9f43',
-}
+import { integerValidator, lengthValidator } from "@validators"
+import { chartJsCustomColors } from "@/components/chartjs/setup/chartJsCustomColors"
 
 const refVForm = ref()
 const processing = ref(false)
-const city = ref('')
+const city = ref(null)
 const formError = ref(null)
 const isFlatSnackbarVisible = ref(false)
 const dataNotfound = ref(false)
 
+onMounted(() => onSubmit())
+
+/**
+ * Function to restore the initial state of the openWeatherResponse object.
+ *
+ * @function restoreInitialState
+ * @returns {void} This function does not return any value.
+ */
 const restoreInitialState = () => {
   openWeatherResponse.name = ''
   openWeatherResponse.country = ''
@@ -47,6 +35,26 @@ const restoreInitialState = () => {
   openWeatherResponse.coord.lon = ''
 }
 
+/**
+ * Represents the response received from the OpenWeather API.
+ *
+ * @typedef {object} OpenWeatherResponse
+ *
+ * @property {string} name - The name of the location.
+ * @property {string} country - The country of the location.
+ * @property {object} weather - The weather conditions at the location.
+ * @property {string} weather.description - The description of the weather.
+ * @property {string} weather.icon - The icon representing the weather.
+ * @property {string} snow - The snow amount at the location.
+ * @property {string} rain - The rain amount at the location.
+ * @property {string} clouds - The cloudiness at the location.
+ * @property {string} humidity - The humidity at the location.
+ * @property {string} sunrise - The time of the sunrise at the location.
+ * @property {string} sunset - The time of the sunset at the location.
+ * @property {object} coord - The geographical coordinates of the location.
+ * @property {string} coord.lat - The latitude of the location.
+ * @property {string} coord.lon - The longitude of the location.
+ */
 const openWeatherResponse = reactive({
   name: '',
   country: '',
@@ -66,6 +74,13 @@ const openWeatherResponse = reactive({
   },
 })
 
+/**
+ * An array that represents supported languages.
+ *
+ * @type {Array<Object>}
+ * @property {string} language - The full name of the language.
+ * @property {string} abbr - The abbreviation code for the language.
+ */
 const languages = [
   {
     language: 'English',
@@ -77,61 +92,119 @@ const languages = [
   },
 ]
 
+/**
+ * Represents a selected language.
+ *
+ * @typedef {Object} SelectedLanguage
+ * @property {string} language - The name of the language.
+ * @property {string} abbr - The abbreviation of the language.
+ */
 const selectedLanguage = ref({
   language: 'Italiano',
   abbr: 'IT',
 })
 
+/**
+ * A reactive variable that contains different filters for weather conditions.
+ *
+ * @type {Reactive<{humidityAndClouds: boolean, rainAndSnow: boolean, temperature: boolean}>}
+ */
 const filters = reactive({
   humidityAndClouds: true,
   rainAndSnow: false,
   temperature: false,
 })
 
+/**
+ * Represents a reactive variable for making a request.
+ *
+ * @type {Reactive<{ q: string, zip: string, lang: string }>}
+ */
 const request = reactive({
   q: 'Torino',
   zip: '',
   lang: computed(() => selectedLanguage.value.abbr.toLowerCase()),
 })
 
+/**
+ * Resets the form by setting values to null or empty string.
+ */
 const onReset = () => {
   formError.value = null
   request.q = ''
   request.zip = ''
 }
 
-onMounted(() => onSubmit())
+/**
+ * Updates the openWeatherResponse object with data from the API response.
+ *
+ * @param {object} response - The API response object.
+ * @returns {void}
+ */
+const handleResponseData = response => {
+  openWeatherResponse.name = response.data.name
+  openWeatherResponse.country = response.data.sys.country
+  openWeatherResponse.weather.description = response.data.weather[0].description
+  openWeatherResponse.weather.icon = response.data.weather[0].icon
+  openWeatherResponse.clouds = response.data.clouds.all
+  openWeatherResponse.humidity = response.data.main.humidity
+  openWeatherResponse.rain = response.data.rain !== undefined ? response.data.rain['1h'] : ''
+  openWeatherResponse.snow = response.data.snow !== undefined ? response.data.rain['1h'] : ''
+  openWeatherResponse.sunrise = new Date(response.data.sys.sunrise * 1000).toLocaleTimeString()
+  openWeatherResponse.sunset = new Date(response.data.sys.sunset * 1000).toLocaleTimeString()
+  openWeatherResponse.coord.lat = response.data.coord.lat
+  openWeatherResponse.coord.lon = response.data.coord.lon
+}
 
+/**
+ * Handles the catch block error and performs the necessary operations.
+ *
+ * @param {Error} error - The error object caught in the catch block.
+ */
+const handleCatch = error => {
+  dataNotfound.value = true
+  formError.value = error.response.data
+  isFlatSnackbarVisible.value = true
+  restoreInitialState()
+}
+
+/**
+ * Resets the processing value and clears the city value.
+ *
+ * @function handleFinally
+ * @returns {void}
+ */
+const handleFinally = () => {
+  processing.value = false
+  city.value = null
+}
+
+/**
+ * Handles the onSubmit event.
+ *
+ * This function validates the form, and if it is valid, it calls the openWeatherRepository's
+ * index method, passing the request object. It then calls handleResponseData with the response,
+ * or handleCatch with the error if an error occurred. Finally, it calls handleFinally.
+ *
+ * @returns {void}
+ */
 const onSubmit = () => {
   formError.value = null
   dataNotfound.value = false
+
   refVForm.value?.validate().then(({ valid: isValid }) => {
     if (isValid) {
       processing.value = true
+
       setTimeout(() => {
         openWeatherRepository().index(request).then(response => {
-          openWeatherResponse.name = response.data.name
-          openWeatherResponse.country = response.data.sys.country
-          openWeatherResponse.weather.description = response.data.weather[0].description
-          openWeatherResponse.weather.icon = response.data.weather[0].icon
-          openWeatherResponse.clouds = response.data.clouds.all
-          openWeatherResponse.humidity = response.data.main.humidity
-          openWeatherResponse.rain = response.data.rain !== undefined ? response.data.rain['1h'] : ''
-          openWeatherResponse.snow = response.data.snow !== undefined ? response.data.rain['1h'] : ''
-          openWeatherResponse.sunrise = new Date(response.data.sys.sunrise * 1000).toLocaleTimeString()
-          openWeatherResponse.sunset = new Date(response.data.sys.sunset * 1000).toLocaleTimeString()
-          openWeatherResponse.coord.lat = response.data.coord.lat
-          openWeatherResponse.coord.lon = response.data.coord.lon
+          handleResponseData(response)
         }).catch(error => {
-          dataNotfound.value = true
-          formError.value = error.response.data
-          isFlatSnackbarVisible.value = true
-          restoreInitialState()
+          handleCatch(error)
         }).finally(() => {
-          processing.value = false
-          city.value = null
+          handleFinally()
         })
-      }, 1000)
+      }, 1000) // Set to show the loader 👉 effect NO PROD
     }
   })
 }
@@ -232,6 +305,7 @@ const onSubmit = () => {
               <VCol
                 v-if="! processing && ! dataNotfound"
                 cols="12"
+                class="d-flex align-center gap-4"
               >
                 <VCheckbox
                   v-model="filters.humidityAndClouds"
@@ -314,9 +388,9 @@ const onSubmit = () => {
                 label-message="Percentage"
                 :data="[openWeatherResponse.humidity, openWeatherResponse.clouds]"
                 :background-color="[
-                chartJsCustomColors.primary,
-                chartJsCustomColors.lineChartWarning,
-              ]"
+                  chartJsCustomColors.primary,
+                  chartJsCustomColors.lineChartWarning,
+                ]"
               />
             </VCardText>
           </VCard>
@@ -345,13 +419,9 @@ const onSubmit = () => {
 </template>
 
 <style lang="scss">
-.date-picker-wrapper {
-  inline-size: 10.5rem;
-}
-
-#chartjs-wrapper {
-  .v-card-item__append {
-    padding-inline-start: 0;
+  #chartjs-wrapper {
+    .v-card-item__append {
+      padding-inline-start: 0;
+    }
   }
-}
 </style>
